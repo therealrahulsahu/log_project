97d51002959257cbddb28d55a8753d6793a1a83d
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-07-14 01:51:42 +0530

    first load

---
+++ code/images/__init__.py
@@ -0,0 +1,6 @@
+from PyQt5.QtGui import QPixmap
+im_correct = QPixmap("correct.ico")
+im_enter = QPixmap("enter.ico")
+im_insert_table = QPixmap("insert_table.ico")
+im_wrong = QPixmap("wrong.ico")
+
