602ffcd297e86239703e7729f75f4ee98dea8c03
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-07-15 18:26:45 +0530

    first load

--- code/mongo_conn/__init__.py
+++ code/mongo_conn/__init__.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient as __client
 myc_localhost = __client("mongodb://localhost:27017/")
-myc_web = __client('mongodb+srv://therealrahulsahu:rahulsahu1_'
-                      '@democluster-2u6fb.gcp.mongodb.net/test?retryWrites=true')
+myc_web = __client('mongodb+srv://therealrahulsahu:rahulsahu1_@democluster-2u6fb.gcp.mongodb.net/test?retryWrites=true')
 
